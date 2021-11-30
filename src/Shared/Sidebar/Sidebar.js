7895a5ae0951dcdd3d59b88ecbc16d083a74aef9
DEWAN <dewanmahinchy@gmail.com>
2021-11-30 18:11:24 +0600

    sidebar added

--- src/Shared/Sidebar/Sidebar.js
+++ src/Shared/Sidebar/Sidebar.js
@@ -1,3 +1,5 @@
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Sidebar.css'
@@ -6,20 +8,20 @@ const Sidebar = () => {
     return (
         <div className="sidebar">
             <div className="myLogo">
-                <h2>
+                <h2 className='m-0'>
                     <NavLink to="/home">
                         <span style={{fontSize: '45px', fontWeight: 'bold'}}>d</span>eewaan<span style={{color:'#CB450C', fontSize: '70px'}}>.</span>
                     </NavLink>
                 </h2>
             </div>
             <div className="sidebar-menu d-flex flex-column">
-                <NavLink to="/home">Home</NavLink>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/projects">Projects</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
+                <NavLink activeStyle={{borderBottom: '2px solid #CB450C', color: '#ebebeb'}} className="l1" to="/home">Home</NavLink>
+                <NavLink activeStyle={{borderBottom: '2px solid #CB450C', color: '#ebebeb'}} className="l2" to="/about">About</NavLink>
+                <NavLink activeStyle={{borderBottom: '2px solid #CB450C', color: '#ebebeb'}} className="l3" to="/projects">Projects</NavLink>
+                <NavLink activeStyle={{borderBottom: '2px solid #CB450C', color: '#ebebeb'}} className="l4" to="/contact">Contact</NavLink>
             </div>
             <div className="sidebar-resume">
-                <button>Resume</button>
+                <a href='https://drive.google.com/uc?export=download&id=1YwdOYEhqqAV5dcspqE7ty4h8J5UnmNtw'><span>Resume</span> <FontAwesomeIcon style={{fontSize: '15px'}} icon={faDownload}/></a>
             </div>
         </div>
     );
